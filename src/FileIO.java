eea07da1b2de45f924e2127ba99634e9cd4ad2b9
stinetorndal <cph-sc95@stud.ek.dk>
2025-11-07 13:52:09 +0100

    Tilføjet muligheder for bruger

---
+++ src/FileIO.java
@@ -0,0 +1,11 @@
+public class FileIO {
+
+    /* readMovieCsv()
+    readShowsCsv()
+    addMovie()
+    addShow()
+    saveUserData()//user, forslag baseret på hvad du har set før, genrer til dig, sidste chance
+                  //nye tilføjede film, notfikationer, downloads, se videre, trailere, indstillinger
+    readUserData()
+     */
+}
