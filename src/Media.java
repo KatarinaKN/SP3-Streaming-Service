d28b2855e55c6276718d8811c6ac1e64e02ae5bc
MathiasStenvall <cph-ms1422@stud.ek.dk>
2025-11-08 10:48:05 +0100

    created Media class

---
+++ src/Media.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public abstract class Media {
+    private String name;
+    private int age;
+    private ArrayList<String> category;
+    private double rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public ArrayList<String> getCategory() {
+        return category;
+    }
+
+    public void setCategory(ArrayList<String> category) {
+        this.category = category;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
